113aa7a89784756ae7e725a08d0b998e59e55f15
kekons4 <kekons4@gmail.com>
2021-02-25 15:03:45 -0500

    added expiry date to cookie and more comments

--- Develop/script.js
+++ Develop/script.js
@@ -9,22 +9,39 @@ var today = moment().format("dddd, MMMM Do YYYY");
 var day = moment().startOf('day');
 
 // initializes cookie
-var savedData = ["", "", "", "", "", "", "", "", ""];
+var savedData = {
+    value: ["", "", "", "", "", "", "", "", ""],
+    expiry: moment().format('dddd, MMMM Do YYYY')
+};
+// Checks if there is a cookie called calItems
+// If not then create one, else grab it and store it in savedData
 if (localStorage.getItem("calItems") === null) {
     localStorage.setItem("calItems", JSON.stringify(savedData));
     savedData = JSON.parse(localStorage.getItem("calItems").split(","));
 } else {
     savedData = localStorage.getItem("calItems");
     savedData = JSON.parse(savedData.split(","));
+
+    // Checks to see if the expiry date is equal to the todays date
+    // If not then it will clear the cookie and reinitialize each time block,
+    // else use the current cookie
+    if (savedData.expiry !== moment().format('dddd, MMMM Do YYYY')) {
+        localStorage.removeItem("calItems");
+        localStorage.setItem("calItems", JSON.stringify({
+            value: ["", "", "", "", "", "", "", "", ""],
+            expiry: moment().format('dddd, MMMM Do YYYY')
+        }));
+        savedData = JSON.parse(localStorage.getItem("calItems").split(","));
+        alert("Yesterday has been cleared");
+    }
 }
-// console.log(savedData);
 
 // generates html for each time block in the calender
 var calender = function () {
     for (let i = 0; i < 9; i++) {
         // Create Time Block
         var timeBlock = $("<div></div>");
-        timeBlock.addClass("timeblock row");
+        timeBlock.addClass("time-block row");
 
         // creates the hour element
         var hour = $("<div></div>");
@@ -34,15 +51,16 @@ var calender = function () {
         // creates the description element
         var description = $("<div></div>");
         description.addClass("textarea description");
-        description.attr("data-time", 9+i)
+        description.attr("data-time", 9 + i)
         // creates the textarea and appends it into the description
         var textArea = $("<textarea id='test' placeholder='Enter Description...'></textarea>");
         // checks if the cookies are empty or not
         if (savedData !== null) {
-            textArea.text(savedData[i]);
+            textArea.text(savedData.value[i]);
         } else {
             textArea.text("");
         }
+        // append textarea to description and description to timeBlock
         textArea.attr("data-value", i);
         description.append(textArea);
         timeBlock.append(description);
@@ -88,16 +106,14 @@ function main() {
         var dataTime = Number.parseInt(container.children().eq(i).children().eq(1).attr("data-time"));
 
         //Determines which timeBlock is what color
-        if(time < dataTime) {
+        if (time < dataTime) {
             container.children().eq(i).children().eq(1).addClass("future")
-        } else if(time === dataTime) {
+        } else if (time === dataTime) {
             container.children().eq(i).children().eq(1).addClass("present")
         } else {
             container.children().eq(i).children().eq(1).addClass("past")
         }
     }
-
-
 }
 
 // captures the event.target of the save button in the time block in .container
@@ -106,11 +122,11 @@ container.on("click", "#save", function (event) {
     var newArr = savedData;
     var index = $(event.target).parent().parent().children().eq(1).children().attr("data-value");
     var text = $(event.target).parent().parent().children().eq(1).children().val();
-    console.log(text);
-    console.log(savedData[index]);
-    newArr[index] = text;
+    // console.log(text);
+    newArr.value[index] = text;
+    // console.log(newArr.value[index]);
     localStorage.setItem("calItems", JSON.stringify(newArr));
-    console.log(localStorage.getItem("calItems"));
+    // console.log(localStorage.getItem("calItems"));
 });
 
 main();
